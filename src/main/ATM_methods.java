e9a5760870bd356f4f42286cb95186e73cb1e79e
RealGJ <34713448+RealGJ@users.noreply.github.com>
2022-05-20 11:18:39 +0530

    Implemented PIN code to replicate ATM security

--- src/main/ATM_methods.java
+++ src/main/ATM_methods.java
@@ -3,6 +3,9 @@ package main;
 import java.util.Scanner;
 
 public class ATM_methods {
+
+	private static int pin = 1234;
+
 	public static void greet() {
 		System.out.println("Welcome to ATM Machine");
 		System.out.println("\n Please Choose on of the Following Function: ");
@@ -12,22 +15,35 @@ public class ATM_methods {
 		System.out.println("4. Quit");
 	}
 
-	public static void withdraw(int balance,int withdraw) {
-		
-		if (balance >= withdraw) {
-			balance = balance - withdraw;
-			System.out.println("Please Collect the Money");
-			System.out.println("Your updated Balance is: " + balance);
+	public static void withdraw(int balance, int withdraw, Scanner scn) {
+
+		System.out.println("Enter Your PIN: ");
+		int enteredPIN = scn.nextInt();
+		if (enteredPIN == pin) {
+
+			if (balance >= withdraw) {
+				balance = balance - withdraw;
+				System.out.println("Please Collect the Money");
+				System.out.println("Your updated Balance is: " + balance);
+			} else {
+				System.out.println("\nNot Enough Balance to Withdraw!!\n");
+			}
+			System.out.println("");
 		} else {
-			System.out.println("\nNot Enough Balance to Withdraw!!\n");
+			System.out.println("Entered PIN is Incorrect! Please try Again!! \n");
 		}
-		System.out.println("");
 	}
-	
-	public static void deposit(int balance,int deposit) {
-		balance = balance + deposit;
-		System.out.println("Money Deposited Sucessfully");
-		System.out.println("Your updated Balance is: " + balance);
-		System.out.println("");
+
+	public static void deposit(int balance, int deposit, Scanner scn) {
+		System.out.println("Enter Your PIN: ");
+		int enteredPIN = scn.nextInt();
+		if (enteredPIN == pin) {
+			balance = balance + deposit;
+			System.out.println("Money Deposited Sucessfully");
+			System.out.println("Your updated Balance is: " + balance);
+			System.out.println("");
+		} else {
+			System.out.println("Entered PIN is Incorrect! Please try Again!! \n");
+		}
 	}
 }
--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -3,46 +3,46 @@ package main;
 import java.util.*;
 
 public class MainClass extends ATM_methods {
-
-//	private static final Scanner scn = null;
+//	private static int pin = 0000;
 
 	public static void main(String[] args) throws Exception {
 		int balance = 50000, withdraw, deposit, num;
 		int sleep = 1500;
 		Scanner scn = new Scanner(System.in);
-		
-		try {
-		mainLoop: while (true) {
-			greet();
-			num = scn.nextInt();
 
-			switch (num) {
-			case 1:
-				System.out.println("Please enter the amount to be withdrawn: ");
-				withdraw = scn.nextInt();
-				withdraw(balance,withdraw);
-				Thread.sleep(sleep);
-				break;
-			case 2:
-				System.out.println("Please enter the amount to be Deposited: ");
-				deposit = scn.nextInt();
-				deposit(balance,deposit);
-				Thread.sleep(sleep);
-				break;
-			case 3:
-				System.out.println("\n Your Current Balance is: " + balance);
-				System.out.println();
-				Thread.sleep(sleep);
-				break;
-			case 4:
-				System.out.println("\nThanks for Visiting ATM \n");
-				break mainLoop;
-			default:
-				break;
+		try {
+			mainLoop: while (true) {
+				greet();
+				num = scn.nextInt();
+
+				switch (num) {
+				case 1:
+					System.out.println("Please enter the amount to be withdrawn: ");
+					withdraw = scn.nextInt();
+					withdraw(balance, withdraw, scn);
+					Thread.sleep(sleep);
+					break;
+				case 2:
+					System.out.println("Please enter the amount to be Deposited: ");
+					deposit = scn.nextInt();
+					deposit(balance, deposit, scn);
+					Thread.sleep(sleep);
+					break;
+				case 3:
+					System.out.println("\n Your Current Balance is: " + balance);
+					System.out.println();
+					Thread.sleep(sleep);
+					break;
+				case 4:
+					System.out.println("\nThanks for Visiting ATM \n");
+					break mainLoop;
+				default:
+					break;
+				}
 			}
-		}
 		} catch (Exception e) {
 			System.out.println("Invalid Request");
+			System.out.println();
 		}
 
 	}
